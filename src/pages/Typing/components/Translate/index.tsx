c31bee95633eba7b8eba59464c6f73e63cc75a67
longpd <longpddev@gmail.com>
2024-03-06 21:11:10 +0700

    update page

--- src/pages/Typing/components/Translate/index.tsx
+++ src/pages/Typing/components/Translate/index.tsx
@@ -1,6 +1,7 @@
 import { TypingContext } from '../../store'
 import Popup from '../Popup'
 import { LoadingUI } from '@/components/Loading'
+import Markdown from '@/components/Markdown'
 import { useContext, useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 import useSWR from 'swr'
@@ -11,29 +12,48 @@ const translateFetch = (url: string) =>
     .then((res) => res.json())
     .then((data) => data.translated_text)
 
+const dictionaryFetch = (url: string) =>
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => data.result as string | null)
+
 const TranslateText = ({ onClose }: { onClose: () => void }) => {
   const { state } = useContext(TypingContext)!
   const currentWord = state.chapterData.words[state.chapterData.index]
 
   const { data, isLoading } = useSWR(`${import.meta.env.VITE_API_URI}/explain?text=${currentWord?.name ?? ''}`, translateFetch)
+  const { data: dictionary } = useSWR(`${import.meta.env.VITE_API_URI}/dictionary?text=${currentWord?.name ?? ''}`, dictionaryFetch)
+  // const bg = currentWord?.name ? `${import.meta.env.VITE_API_URI}/visualize?text=${currentWord.name ?? ''}` : ''
+  console.log('🚀 ~ TranslateText ~ dictionary:', dictionary)
   return (
-    <>
-      <div className="relative flex items-end justify-between rounded-t-lg border-b border-neutral-100 bg-stone-50 px-6 py-3 dark:border-neutral-700 dark:bg-gray-900">
+    <div className="flex max-h-[95vh] flex-col">
+      <div className="relative flex items-end justify-between rounded-t-lg  bg-stone-50 px-6 py-3 dark:bg-gray-900">
         <span className="text-3xl font-bold text-gray-600">{currentWord?.name}</span>
         <button type="button" onClick={onClose} title="Close dialog">
           <IconX className="absolute right-7 top-5 cursor-pointer text-gray-400" />
         </button>
       </div>
-      <div className="min-h-32 whitespace-break-spaces p-4 text-left">
+      <div className="relative min-h-32 overflow-auto p-4 text-left">
+        {/* <img
+          src={bg}
+          alt=""
+          className="pointer-events-none absolute inset-0 block h-full w-full object-cover object-center opacity-20"
+          style={{ filter: 'blur(2px) brightness(0.5)' }}
+        /> */}
+        {dictionary && (
+          <div>
+            <Markdown>{dictionary}</Markdown>
+          </div>
+        )}
         {isLoading ? (
           <div className="flex h-full w-full items-center justify-center">
             <LoadingUI />
           </div>
         ) : (
-          data
+          <Markdown>{data}</Markdown>
         )}
       </div>
-    </>
+    </div>
   )
 }
 
